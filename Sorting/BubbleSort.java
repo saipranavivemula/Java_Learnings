58a9451910ce04efae82a855103e14ed5f11f72e
saipranavivemula <saipranavivemula6@gmail.com>
2026-09-03 13:05:34 +0530

    Create BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+public class Sorting {
+	public static void main(String[] args) {
+		int[] arr= {1,2,5,3,4};
+		bubblesort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+	public static void bubblesort(int[] arr) {
+		for(int i=0;i<arr.length;i++) {
+			boolean swap=false;
+			for(int j=1;j<arr.length-i;j++) {
+				if(arr[j]<arr[j-1]) {
+					int temp=arr[j];
+					arr[j]=arr[j-1];
+					arr[j-1]=temp;
+					swap=true;
+				}
+			}
+			if(!swap) {
+				break;
+			}
+		}
+	}
+}
