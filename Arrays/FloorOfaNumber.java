edd8c086076f9951636588b11a32758ec0b36c7c
saipranavivemula <saipranavivemula6@gmail.com>
2026-09-03 12:34:14 +0530

    Create FloorOfaNumber.java

---
+++ Arrays/FloorOfaNumber.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class BinarySearch {
+  public static void main(String args[]) {
+  Scanner sc=new Scanner(System.in);
+  int[] arr= {1,4,6,9,12,22,34,45,67,88};
+  System.out.println("Enter the element you want to search:");
+  int target=sc.nextInt();
+  int index=binary(arr,target);
+  System.out.println("ele="+index);
+  }
+  public static int binary(int[] arr,int target) {
+    int start=0;
+    int end=arr.length-1;
+    while(start<=end) {
+	    int mid=start+((end-start)/2);
+	    if(target>arr[mid]) {
+		    start=mid+1;
+	    }else if(target<arr[mid]) {
+		    end=mid-1;
+	    }else {
+		    return mid;
+	}
+  }
+  return end;
+}
+}
